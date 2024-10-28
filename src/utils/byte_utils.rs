98c864530156df6422666e7acbe5c6484c1392d1
narad-muni <saumillinux@gmail.com>
2024-10-28 18:21:28 +0530

    Reduce unsafe block size

--- src/utils/byte_utils.rs
+++ src/utils/byte_utils.rs
@@ -1,13 +1,15 @@
 use std::{
-    any::type_name, mem::{self, ManuallyDrop, MaybeUninit}, ptr
+    any::type_name,
+    mem::{self, ManuallyDrop, MaybeUninit},
+    ptr,
 };
 
 use crate::constants::BUF_SIZE;
 
 pub fn bytes_to_struct<T>(s: &[u8]) -> T {
-    unsafe {
-        let src = s.as_ptr() as *const T;
+    let src = s.as_ptr() as *const T;
 
+    unsafe {
         std::ptr::read(src)
     }
 }
@@ -22,7 +24,7 @@ pub fn struct_to_bytes<T>(s: &T, buffer: &mut [u8]) -> usize {
 
     // Get a pointer to the value
     let ptr = s as *const T as *const u8;
-    
+
     unsafe {
         // Copy the bytes into the buffer
         std::ptr::copy_nonoverlapping(ptr, buffer.as_mut_ptr(), size);
@@ -34,7 +36,13 @@ pub fn struct_to_bytes<T>(s: &T, buffer: &mut [u8]) -> usize {
 // Safe because of assert
 pub fn cast<T, F>(input: F) -> T {
     // Ensure that T and U have the same size to avoid undefined behavior
-    assert_eq!(std::mem::size_of::<T>(), std::mem::size_of::<F>(), "Cannot cast from smaller struct {} to larger struct {}", type_name::<F>(), type_name::<T>());
+    assert_eq!(
+        std::mem::size_of::<T>(),
+        std::mem::size_of::<F>(),
+        "Cannot cast from smaller struct {} to larger struct {}",
+        type_name::<F>(),
+        type_name::<T>()
+    );
 
     // Use `ManuallyDrop` to take ownership of `input` without dropping it
     let input = ManuallyDrop::new(input);
@@ -43,16 +51,14 @@ pub fn cast<T, F>(input: F) -> T {
     unsafe { ptr::read(&*input as *const F as *const T) }
 }
 
-
 // Allows to cast from smaller structure to larger structure
 // Accessing field outside of F's size will cause segfault
 pub unsafe fn cast_unsafe<F, T>(input: F) -> T {
-
     // Use `ManuallyDrop` to take ownership of `input` without dropping it
     let input = ManuallyDrop::new(input);
 
     // Use `ptr::read` to reinterpret the bytes of `input` as type `U`
-    unsafe { ptr::read(&*input as *const F as *const T) }
+    ptr::read(&*input as *const F as *const T)
 }
 
 pub fn uninit_to_buf(src: &[MaybeUninit<u8>; BUF_SIZE]) -> [u8; BUF_SIZE] {
@@ -64,18 +70,19 @@ pub fn create_empty<T>() -> T {
 }
 
 pub fn bytes_to_partial_struct<T>(s: &mut T, buffer: &[u8]) {
+    // Get unsafe mutable raw pointer
+    let struct_ptr = s as *mut T as *mut u8;
+
+    // Copy lowest size
+    // This is to prevent overflow
+    let mut min_size = mem::size_of::<T>();
+
+    if min_size > buffer.len() {
+        min_size = buffer.len();
+    }
+
+    
     unsafe {
-        // Get unsafe mutable raw pointer
-        let struct_ptr = s as *mut T as *mut u8;
-
-        // Copy lowest size
-        // This is to prevent overflow
-        let mut min_size = mem::size_of::<T>();
-
-        if min_size > buffer.len() {
-            min_size = buffer.len();
-        }
-
         // Similar to memcpy
         ptr::copy_nonoverlapping(buffer.as_ptr(), struct_ptr, min_size);
     };
