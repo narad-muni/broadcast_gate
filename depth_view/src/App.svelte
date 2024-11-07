bb0f74c6404bc1f6ed55c74bce54c8901ca9030d
narad-muni <--local>
2024-11-07 09:45:26 +0530

    Svelte not updating

--- depth_view/src/App.svelte
+++ depth_view/src/App.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
   import Details from "./details.svelte";
 
-  let search = $state("");
+  let search = $state(localStorage.getItem("search") || "");
   let tokens = $derived(search.split(" "));
   let depth_data = $state({});
   let ws_url = $state(localStorage.getItem("ws_url") || "ws://localhost:8080");
+
+  $effect(() => {
+    localStorage.setItem("search", search);
+  });
 
   $effect(() => {
     localStorage.setItem("ws_url", ws_url);
   });
 
   function add_data(key, data) {
+    console.log("Received ", key);
+
     key = key.toString();
     
     if (data.buy_depth_count == 0 && data.sell_depth_count == 0) {
@@ -21,7 +27,12 @@
       return
     }
 
+    delete depth_data[key];
     depth_data[key] = data;
+
+    if (key == "434648" || key == 434648) {
+      console.log(depth_data);
+    }
   }
 
   document.addEventListener("depth_data_event", (event) => {
@@ -80,15 +91,18 @@
       <input bind:value={search} type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="429390 429392" required />
   </div>
 </form>
+
 <main class="flex flex-wrap justify-evenly">
 
-  {#each Object.keys(depth_data) as key}
-    {#if tokens.includes(key) || search == ""}
-      <Details data={depth_data[key]}/>
-    {/if}
-  {/each}
 
-  {#if Object.keys(depth_data).length == 0}
-  <h1>No data to display</h1>
-  {/if}
+{#each Object.entries(depth_data) as [key, data]}
+{console.log(tokens.includes(key), tokens, key)}
+    {#if tokens.includes(key) || search == ""}
+    <Details {data}/>
+    {/if}
+{/each}
+
+{#if Object.keys(depth_data).length == 0}
+<h1>No data to display</h1>
+{/if}
 </main>
